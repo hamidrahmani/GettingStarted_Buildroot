0296812bf225dad7f2d523f082baf713c2c7b68f
hamidrahmani <hamid_rahmani80@yahoo.com>
2026-01-22 12:55:51 +0100

    delete some unneccassirly files

--- scripts/run_keysign_generator.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-./make_keys_and_sign.sh --image ../buildroot/output/images/zImage --overlay ../overlays/beaglebone/rootfs-overlay/boot/
-
-../scripts/make_keys_and_sign.sh --image output/images/zImage --overlay ../overlays/beaglebone/rootfs-overlay/boot/
